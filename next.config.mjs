9e18b43737a50770eee0b69dee28342d201ebd27
rickyhung <rickyzero@outlook.com>
2025-04-15 22:16:47 +0800

    pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,      // Enable React strict mode for improved error handling
+    compiler: {
+        removeConsole: process.env.NODE_ENV !== "development"     // Remove console.log in production
+    }
+};
+
+export default withPWA({
+    dest: "public",         // destination directory for the PWA files
+    disable: false,        // disable PWA in the development environment (process.env.NODE_ENV === "development")
+    register: true,         // register the PWA service worker
+    skipWaiting: true,      // skip waiting for service worker activation
+})(nextConfig);
