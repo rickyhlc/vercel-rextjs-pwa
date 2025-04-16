1cc2763d96b6270e6a7ba2f80cb7560451292497
rickyhung <rickyzero@outlook.com>
2025-04-16 19:03:10 +0800

    use warn

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -9,8 +9,8 @@ export default function AuthCallbackPage() {
   useEffect(() => {
     const hashParams = new URLSearchParams(window.location.hash.substring(1));
     const token = hashParams.get("id_token");
-    console.log(hashParams);
-    console.log(token);
+    console.warn(hashParams);
+    console.warn(token);
     if (token) {
       localStorage.setItem("googleAuthToken", token);
       router.push("/");
