7ca0e9ac2cf21575004dca7d4a599cbb9eac9924
rickyhung <rickyzero@outlook.com>
2025-04-17 11:40:33 +0800

    log

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -7,7 +7,8 @@ export default function AuthCallbackPage() {
   const router = useRouter();
 
   useEffect(() => {
-    console.log(window.location);
+    console.log(window.location.href);
+    console.log(window.location.hash);
     const hashParams = new URLSearchParams(window.location.hash.substring(1));
     const token = hashParams.get("id_token");
     if (token) {
