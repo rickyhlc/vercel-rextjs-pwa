6ae30b5a09b2fe4cd15754530758fdf83fdee697
rickyhung <rickyzero@outlook.com>
2025-04-30 17:36:24 +0800

    redirect

--- src/middleware.js
+++ src/middleware.js
@@ -4,18 +4,18 @@ import { auth } from "./auth/authConfig";
 export default async function middleware(request) {
     const session = await auth();
 
-    const { pathname, origin } = request.nextUrl;
+    const { pathname } = request.nextUrl;
 
-    console.log("middleware runs for ", request.url, pathname, origin, new URL("/signIn", request.url));
+    console.log("middleware runs for ", pathname);
 
     if (session?.user?.email) {
         if (process.env.VALID_EMAILS.split(",").includes(session.user.email)) {
             return NextResponse.next();
         }
     }
-    return NextResponse.redirect(new URL(`${origin}/signIn`, request.url)); 
+    return NextResponse.redirect(new URL("/signIn", request.url)); 
 }
 
 export const config = {
-    matcher: ["/home"],
+    matcher: ["/", "/home"],
 }
