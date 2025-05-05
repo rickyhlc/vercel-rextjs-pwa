d5aa90ee6cc38d8b518412c9af522275fd86998a
rickyhung <rickyzero@outlook.com>
2025-05-05 22:22:07 +0800

    test select

--- src/app/camera/page.js
+++ src/app/camera/page.js
@@ -48,6 +48,17 @@ export default function CameraPage() {
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 6H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1Zm7 11-6-2V9l6-2v10Z"/>
                 </svg>
             </label>
+            <label className="rounded bg-blue-600 p-2.5 text-center text-white active:bg-blue-800 hover:bg-blue-700">
+                <input
+                    accept="video/*"
+                    className="hidden"
+                    type="file"
+                    onChange={(e) => handleCapture(e.target)}
+                />
+                <svg className="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 6H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1Zm7 11-6-2V9l6-2v10Z"/>
+                </svg>
+            </label>
         </div>
         {source && <img src={source} alt={"Preview!~"} className=""></img>}
     </div>
