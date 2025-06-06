9e29850ff2c958b7272b1595e6474584e57ca27f
rickyhung <rickyzero@outlook.com>
2025-06-06 21:53:19 +0800

    test action

--- public/push-sw.js
+++ public/push-sw.js
@@ -4,12 +4,15 @@ self.addEventListener("push", (event) => {
   const data = event.data.json();
   event.waitUntil(self.registration.showNotification(data.title, {
     body: data.text,
+    icon: "/icon-192x192.png",
+    actions: ["ook", "go"]
   }));
 });
 
-// self.addEventListener("notificationclick", (event) => {
-//   const data = event.data.json();
-//   self.registration.showNotification(data.title, {
-//     body: data.text,
-//   });
-// });
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  const data = event.data.json();
+  self.registration.showNotification(data.title, {
+    body: data.text + "__" + event.action,
+  });
+});
