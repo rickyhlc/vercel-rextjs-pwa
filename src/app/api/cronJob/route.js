4dfac9abdfbd41eef7226b8614394511be5c2107
rickyhung <rickyzero@outlook.com>
2025-07-02 18:31:28 +0800

    relocate file

--- src/app/api/cronJob/route.js
+++ src/app/api/cronJob/route.js
@@ -2,6 +2,6 @@
 export async function GET(request) {
   console.log("Vercel trigger cron job now...");
 
-  return Response.json({ message: "Triggered!" });
+  return Response.json({ ok: true });
 
 }
