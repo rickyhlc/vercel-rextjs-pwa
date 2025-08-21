4caa6e3005587e5848d140e2f7c2a650f5baf4ed
rickyhung <rickyzero@outlook.com>
2025-08-21 11:59:08 +0800

    try useEffect to defer long loading list

--- src/app/bus/search/routeList.js
+++ src/app/bus/search/routeList.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { startTransition } from "react";
+import { useEffect, useState } from "react";
 import RouteItem from "./routeItem";
 import { useDataContext } from "./dataProvider";
 import { CircularProgress } from '@mui/material';
@@ -11,20 +11,24 @@ export default function RouteList() {
 
   const { filteredList } = useDataContext();
   
-  let elm = <div className="text-center mt-20"><CircularProgress size={32} /></div>;
-  startTransition(() => {
-    elm = filteredList.map(r => 
-      <RouteItem
-        key={`${r.route}-${r.bound}-${r.service_type}`}
-        company={"KMB"}
-        route={r.route}
-        bound={r.bound}
-        serviceType={r.service_type}
-        dest={r.dest_tc}
-        orig={r.orig_tc}
-      />
-    )}
-  );
+  const [elm, setElm] = useState(<div className="text-center mt-20"><CircularProgress size={32} /></div>);
+  useEffect(() => {
+    setElm(
+      <>
+        {filteredList.map(r => (
+          <RouteItem
+            key={`${r.route}-${r.bound}-${r.service_type}`}
+            company={"KMB"}
+            route={r.route}
+            bound={r.bound}
+            serviceType={r.service_type}
+            dest={r.dest_tc}
+            orig={r.orig_tc}
+          />
+        ))}
+      </>
+    );
+  }, [filteredList]);
 
   if (filteredList.error) {
     return <div className="text-center mt-20">Unable to load routes...</div>;
