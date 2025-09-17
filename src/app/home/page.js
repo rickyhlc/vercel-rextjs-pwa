bcd92e51bc34e965a86656ad614d75cdf04d73bf
rickyhung <rickyzero@outlook.com>
2025-09-17 16:04:03 +0800

    test wa

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -76,7 +76,10 @@ export default function HomePage() {
       <div className="flex gap-8 items-center">
         <button className="bg-blue-700 text-white w-30 py-2 rounded hover:bg-blue-800 active:bg-blue-900" onClick={() => router.push("/test/testClient")}>Test</button>
         <button className="bg-blue-700 text-white w-30 py-2 rounded hover:bg-blue-800 active:bg-blue-900" onClick={() => router.push("/test/camera")}>Test Camera</button>
+      </div>
+      <div className="flex gap-8 items-center">
         <button className="bg-blue-700 text-white w-30 py-2 rounded hover:bg-blue-800 active:bg-blue-900" onClick={testPushNotification}>Test Web Push</button>
+        <a className="text-center bg-blue-700 text-white w-30 py-2 rounded hover:bg-blue-800 active:bg-blue-900" href="https://wa.me/85261263612">wa</a>
       </div>
       <div className="">{expTime ? dateFormat(expTime) + " " + timeFormat(expTime, "second") : "-"} ({count})</div>
       <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
@@ -91,80 +94,7 @@ export default function HomePage() {
         Save and see your changes instantly.
         </li>
       </ol>
-
-      <div className="flex gap-4 items-center flex-col sm:flex-row">
-        <a
-        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-        >
-        <Image
-          className="dark:invert"
-          src="/vercel.svg"
-          alt="Vercel logomark"
-          width={20}
-          height={20}
-        />
-        Deploy now
-        </a>
-        <a
-        className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-        >
-        Read our docs
-        </a>
-      </div>
     </main>
-    <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-    <a
-      className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-      href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <Image
-      aria-hidden
-      src="/file.svg"
-      alt="File icon"
-      width={16}
-      height={16}
-      />
-      Learn
-    </a>
-    <a
-      className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-      href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <Image
-      aria-hidden
-      src="/window.svg"
-      alt="Window icon"
-      width={16}
-      height={16}
-      />
-      Examples
-    </a>
-    <a
-      className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-      href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <Image
-      aria-hidden
-      src="/globe.svg"
-      alt="Globe icon"
-      width={16}
-      height={16}
-      />
-      Go to nextjs.org →
-    </a>
-    </footer>
   </div>
   );
 }
