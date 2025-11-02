965def9273bc4ddf76b358e410c8e2fb0d62aabd
rickyhung <rickyzero@outlook.com>
2025-11-02 17:50:47 +0800

    grid layout

---
+++ src/app/test/layout/page.js
@@ -0,0 +1,24 @@
+import "./layout.css";
+
+export default function LayoutPage() {
+
+  return (
+    <div className="container">
+      <div className="header">
+        <h2>My Header</h2><p>New Balance sneakers are a popular brand among PEOPLE staffers and celebrities alike (hi, Taylor Swift)</p>
+      </div>
+      <div>
+        <h3>Item 1</h3><p>Alex makes a lot of mistakes in this episode, including following a conspiracy theorist named Maurice (oh hi, Mark McKinney!) to his off-the-grid house to gather more information about the 18 teens who disappeared from Tall Pines, then brutally beating him before fleeing the scene.</p>
+      </div>
+      <div>
+        <h3>Item 2</h3><p>Her lifelike appearance makes her impressive, but even Sophia [a robot] isn't demonstrating true AI. She's not autonomous, and can't make her own decisions.</p>
+      </div>
+      <div>
+        <h3>Item 3</h3><p>Consumers are already comfortable with the use of AI in many of the technologies we use daily.</p>
+      </div>
+      <div>
+        <h3>Item 4</h3><p>The immediate danger, he said, was A.I.'s ability to create convincing false photos, videos and audio—showing things that didn't happen or weren't done by the people seen or heard in the generated content.</p>
+      </div>
+    </div>
+  );
+}
