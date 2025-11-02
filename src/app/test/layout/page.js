d401fa2fac0299746ca19f6659c02b51da82c00e
rickyhung <rickyzero@outlook.com>
2025-11-02 18:00:41 +0800

    escape char

--- src/app/test/layout/page.js
+++ src/app/test/layout/page.js
@@ -11,13 +11,13 @@ export default function LayoutPage() {
         <h3>Item 1</h3><p>Alex makes a lot of mistakes in this episode, including following a conspiracy theorist named Maurice (oh hi, Mark McKinney!) to his off-the-grid house to gather more information about the 18 teens who disappeared from Tall Pines, then brutally beating him before fleeing the scene.</p>
       </div>
       <div>
-        <h3>Item 2</h3><p>Her lifelike appearance makes her impressive, but even Sophia [a robot] isn't demonstrating true AI. She's not autonomous, and can't make her own decisions.</p>
+        <h3>Item 2</h3><p>Her lifelike appearance makes her impressive, but even Sophia isn&apos;t demonstrating true AI. She&apos;s not autonomous, and can&apos;t make her own decisions.</p>
       </div>
       <div>
         <h3>Item 3</h3><p>Consumers are already comfortable with the use of AI in many of the technologies we use daily.</p>
       </div>
       <div>
-        <h3>Item 4</h3><p>The immediate danger, he said, was A.I.'s ability to create convincing false photos, videos and audio—showing things that didn't happen or weren't done by the people seen or heard in the generated content.</p>
+        <h3>Item 4</h3><p>The immediate danger, he said, was A.I.&apos;s ability to create convincing false photos, videos and audio—showing things that didn&apos;t happen or weren&apos;t done by the people seen or heard in the generated content.</p>
       </div>
     </div>
   );
